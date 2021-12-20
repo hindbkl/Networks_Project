0450f87c77ecfa431ef3a2aa1768a64f19737dfe
hindbkl <hbakkalit@gmail.com>
2021-12-20 14:46:53 +0100

    Connection to db works

---
+++ src/Utils/generateTables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE if not exists messages (
+    sender varchar,         -- its username
+    receiver varchar,       -- its username
+    content varchar,
+    timestamp timestamp,
+    SKsender varchar,       -- symmetric key encrypted with sender public key
+    SKreceiver varchar      -- symmetric key encrypted with receiver public key
+);
+
+CREATE TABLE if not exists users (
+    username varchar,
+    password varchar,
+    publickey varchar
+)
